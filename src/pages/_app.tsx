592e66dbb40d5677e76e7d352242dfc6ade19c37
Brenosalv <brenosalvadordefreitas@gmail.com>
2023-12-29 14:31:21 -0300

    feat: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
+import logoImg from "@/assets/logo.svg"
+import { globalStyles } from '@/styles/global'
 import '@/styles/globals.css'
+import { Container, Header } from "@/styles/pages/app"
 import type { AppProps } from 'next/app'
 
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
